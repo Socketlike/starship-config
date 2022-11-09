8f3fe14a4a5c9ec34290574a01b45b73378b8440
SkiddledGitHub <36399055+Abstructor@users.noreply.github.com>
2022-11-09 19:30:05 +0700

    ✨ Add more languages to detection

--- config.h
+++ config.h
@@ -11,11 +11,17 @@ struct Language
   char* extmatch[100];
 };
 
-// Insert your custom language matching here
-struct Language lang[12] = {
-  /* name     count   dir match            file dirmatch                                       extension match */
+/** 
+ * Insert your custom language matching here
+ * Do not set count to anything other than 0!
+ * It is used internally.
+ **/
+struct Language lang[19] = {
+  /* name     count   dir match            file match                                          extension match */
   {"c",       0,      {},                  {},                                                 {".c", ".h"}},
   {"cmake",   0,      {},                  {"CMakeCache.txt", "CMakeLists.txt"},               {}},
+  {"crystal", 0,      {},                  {"shard.yml"},                                      {".cr"}},
+  {"dart",    0,      {".dart_tool"},      {"pubspec.yaml", "pubspec.yml", "pubspec.lock"},    {".dart"}},
   {"dotnet",  0,      {},                  {".csproj", ".fsproj", ".xproj"},                   {"global.json", "project.json", "Directory.Build.props", "Directory.Build.targets", "Packages.props"}},
   {"elixir",  0,      {},                  {},                                                 {"mix.exs"}},
   {"elm",     0,      {"elm-stuff"},       {"elm.json", "elm-package.json", ".elm-version"},   {}},
@@ -24,7 +30,14 @@ struct Language lang[12] = {
   {"java",    0,      {},                  {"pom.xml", "build.gradle.kts", "build.sbt", ".java-version", ".deps.edn", "project.clj", "build.boot"},
                                                                                                {".java", ".jar", ".class", ".cljs", ".cljc"}},
   {"julia",   0,      {},                  {"Manifest.toml", "Project.toml"},                  {".jl"}},
+  {"kotlin",  0,      {},                  {},                                                 {".kt", ".kts"}},
+  {"lua",     0,      {"lua"},             {".lua-version"},                                   {".lua"}},
   {"nodejs",  0,      {"node_modules"},    {"package.json", ".node-version"},                  {".js", ".cjs", ".mjs", ".ts", ".mts", ".cts"}},
+  {"perl",    0,      {},                  {"Makefile.PL", "Build.PL", "cpanfile", "cpanfile.snapshot", "META.json", "META.yml", ".perl-version"},
+                                                                                               {".pl", ".pm", ".pod"}},
+  {"python",  0,      {},                  {".python-version", "Pipfile", "__init__.py", "pyproject.toml", "requirements.txt", "setup.py", "tox.ini"},
+                                                                                               {".py"}},
   {"nimble",  0,      {},                  {"nim.cfg"},                                        {".nim", ".nims", ".nimble"}},
-  {"rust",    0,      {},                  {"Cargo.toml"},                                     {".rs"}}
+  {"rust",    0,      {},                  {"Cargo.toml"},                                     {".rs"}},
+  {"ruby",    0,      {},                  {"Gemfile", ".ruby-version"},                       {".rb"}}
 };
